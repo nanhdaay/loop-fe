2db3ecb31bae9d1444498fc4e2d9d236d037cc47
DangNguyen <nguyendangit04@gmail.com>
2023-09-20 23:54:28 +0700

    fix: Store credential

--- src/axios.js
+++ src/axios.js
@@ -2,4 +2,5 @@ import axios from "axios";
 
 export const makeRequest = axios.create({
   baseURL: "https://loop-be.onrender.com/api/",
+  withCredentials: true
 });
--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,24 +1,30 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 
-export const AuthContext = createContext()
+export const AuthContext = createContext();
 
-export const AuthContextProvider = ({children}) =>{
-    const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("user")) || null);
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
 
-    const login = async(inputs) => {
-        const res = await axios.post("https://loop-be.onrender.com/api/auth/login", inputs);
-
-    setCurrentUser(res.data)
-    };
-
-    useEffect(() =>{
-      localStorage.setItem("user",JSON.stringify(currentUser))
-    },[currentUser]);
-
-    return(
-        <AuthContext.Provider value={{currentUser, login}}>
-            {children}
-        </AuthContext.Provider>
+  const login = async (inputs) => {
+    const res = await axios.post(
+      "https://loop-be.onrender.com/api/auth/login",
+      inputs,
+      { withCredentials: true }
     );
+
+    setCurrentUser(res.data);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
